a6808ae94e990fbeee3729949eacf071ee492e00
Edward Wei <nyw6dh@virginia.edu>
2025-01-08 15:26:21 -0700

    update type

--- src/util/result.ts
+++ src/util/result.ts
@@ -1,3 +1,3 @@
-export type Result<V> =
-	| { variant: "ok"; value: V }
-	| { variant: "error"; error: string };
+export type Result<T = undefined> = T extends undefined
+	? { variant: "ok" } | { variant: "error"; error: string }
+	: { variant: "ok"; value: T } | { variant: "error"; error: string };
