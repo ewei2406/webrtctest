3e5ff61e84b4593b67b7cc66f964fff6b07723a8
Edward Wei <nyw6dh@virginia.edu>
2025-01-09 01:50:47 -0700

    allow nonlocal

--- src/routes/Sender.tsx
+++ src/routes/Sender.tsx
@@ -7,15 +7,13 @@ import useCommunication from "../hooks/useCommunication";
 
 const Sender = () => {
 	const [callerId, setCallerId] = useState<string>("");
+	const [localOnly, setLocalOnly] = useState(true);
 
 	const { messages, addMessage } = useChat();
 
 	const { id, sendMessage, connectionState, call, status } = useCommunication({
-		localOnly: true,
-		onMessage: (m) => {
-			addMessage(m);
-			console.log(m);
-		},
+		localOnly,
+		onMessage: addMessage,
 		receptor: false,
 	});
 
@@ -27,19 +25,27 @@ const Sender = () => {
 		<>
 			<NavLink to="/">Home</NavLink>
 			<h1>Sender</h1>
-			<p>Connection state: {connectionState.variant}</p> <br />
+			<p>Connection state: {connectionState.variant}</p>
+			<input
+				type="checkbox"
+				name="localOnly"
+				checked={localOnly}
+				onChange={() => setLocalOnly(!localOnly)}
+			/>
+			<label htmlFor="localOnly">Use Local Network Only</label> <br />
 			<p>
 				My id: <input type="text" readOnly value={id} />
-				<input
-					type="text"
-					value={callerId}
-					onChange={(e) => setCallerId(e.target.value)}
-				/>
-				<button onClick={handleCall}>Call</button>
 			</p>
+			<input
+				type="text"
+				value={callerId}
+				onChange={(e) => setCallerId(e.target.value)}
+			/>
+			<button onClick={handleCall}>Call</button>
 			<p style={{ color: "red" }}>
 				{status.variant === "error" && status.error}
 			</p>
+			<h3>Chat</h3>
 			<ChatHistory messages={messages} />
 			<ChatInput onSubmit={sendMessage} />
 		</>
