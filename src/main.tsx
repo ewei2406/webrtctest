9ec8ad6b41b51e73d846719b641f13dd02262716
Edward Wei <nyw6dh@virginia.edu>
2025-01-12 00:48:36 -0500

    cleanup unused

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import App from "./App.tsx";
-import Sender from "./routes/Sender.tsx";
-import Receiver from "./routes/Receiver.tsx";
-import Host from "./routes/Host.tsx";
-import Client from "./routes/Client.tsx";
+import App from "./App";
+import Host from "./routes/Host";
+import Client from "./routes/Client";
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
@@ -12,8 +10,6 @@ createRoot(document.getElementById("root")!).render(
 			<Route path="/" element={<App />} />
 			<Route path="/host" element={<Host />} />
 			<Route path="/client" element={<Client />} />
-			<Route path="/send" element={<Sender />} />
-			<Route path="/recv" element={<Receiver />} />
 		</Routes>
 	</BrowserRouter>
 );
